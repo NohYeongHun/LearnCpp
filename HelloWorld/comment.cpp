f3eae587515838e18df1e441b699ab50fc5bbf2b
NYH <dkzndk1100@naver.com>
2023-06-23 17:36:38 +0900

    주석

---
+++ HelloWorld/comment.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	/* multi-line-comment : 다중 라인 주석
+	* 다중 라인 주석 예시
+	*/
+	std::cout << "Hello world!" << std::endl; // single-line-comment : 한줄 주석
+
+
+	return 0;
+}
